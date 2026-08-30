96f8299df6889f3c26257c09f68c748ab6d7dafa
zquere <lamondlamon2@gmail.com>
2026-08-30 10:51:24 +0530

    Clean up Exercise.py and add explanatory comment
    Removed HTML entities and added a comment for clarity.

--- Day-01/Exercise.py
+++ Day-01/Exercise.py
@@ -1,3 +1,4 @@
+# this is normal mean finder see and get better understanding of lis,loop,function
 lis = [20,30,10,40,50,60,70,80,90,100,33,87,67,56,87,5,4,46,34,57,86,97,97,67,56,56,98,76,56,45,]
 top = []
 average =[]
